747f98382e8bff08317f4f3f18301b7b85b17d4f
Eren <drglitch.off@gmail.com>
2024-11-07 21:10:08 +0530

    Add files via upload

---
+++ bot/config.py
@@ -0,0 +1,24 @@
+from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv("config.env")
+
+
+class Telegram:
+    API_ID = int(getenv("API_ID", "12862156"))
+    API_HASH = getenv("API_HASH", "d913a2ef10183c683144ff851481d9fd")
+    BOT_TOKEN = getenv("BOT_TOKEN", "")
+    PORT = int(getenv("PORT", 8080))
+    SESSION_STRING = getenv("SESSION_STRING", "")
+    BASE_URL = getenv("BASE_URL", "https://152.67.176.116").rstrip('/')
+    DATABASE_URL = getenv("DATABASE_URL", "mongodb+srv://muthu2:muthu2@cluster0.u0sdu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    AUTH_CHANNEL = [channel.strip() for channel in getenv("AUTH_CHANNEL", " -1002422001370" ).split(",")]
+    THEME = getenv("THEME", "quartz").lower()
+    USERNAME = getenv("USERNAME", "admin")
+    PASSWORD = getenv("PASSWORD", "admin")
+    ADMIN_USERNAME = getenv("ADMIN_USERNAME", "surfTG")
+    ADMIN_PASSWORD = getenv("ADMIN_PASSWORD", "surfTG")
+    SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
+    WORKERS = int(getenv('WORKERS', '10'))
+    MULTI_CLIENT = bool(getenv('MULTI_CLIENT', 'False'))
+    HIDE_CHANNEL = bool(getenv('HIDE_CHANNEL', 'False'))
